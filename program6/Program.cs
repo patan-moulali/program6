b181c03a363a57a268252f648d916d58b8837684
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-18 15:31:59 +0530

    Add project files.

---
+++ program6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = 0;
+            double inr = 0;
+            double value = 0;
+
+            Console.Write("Enter amount in USD: ");
+            usd = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the USD value :");
+            value = double.Parse(Console.ReadLine());
+
+            inr = usd * value;
+            Console.WriteLine("USD " + usd + "-> INR " + inr);
+        }
+    }
+}
